8b421a0efbd59e77d8491f46328fcac0367df647
Andrey Shevchenko <andrey.shevchenko@sysgears.com>
2013-12-25 16:47:40 +0200

    Add Sphinx images, stylesheets and JS files.

--- SiteConfig.groovy
+++ SiteConfig.groovy
@@ -29,8 +29,8 @@ environments {
 
 // Default features configuration.
 features {
-    compass = 'auto'             // 'none', 'ruby', 'jruby', 'shell', 'auto'
-    highlight = 'pygments'           // 'none', 'pygments'
+    compass = 'none'             // 'none', 'ruby', 'jruby', 'shell', 'auto'
+    highlight = 'pygments'       // 'none', 'pygments'
     // pygments = 'auto'         // 'none', 'python', 'jython', 'shell', 'auto'
     // cache_highlight = 'true'  // default to 'true'
 }
@@ -80,3 +80,78 @@ published: true
 ---
 """)}
 ]
+
+// Theme configuration
+
+/*
+ * The delimiter for the items on the left side of the related bar.
+ * This defaults to ' &raquo;' Each item in the related bar ends
+ * with the value of this variable.
+ */
+reldelim1 = ' &raquo;'
+
+/*
+ * The delimiter for the items on the right side of the related bar.
+ * This defaults to ' |'. Each item except of the last one in the
+ * related bar ends with the value of this variable.
+ */
+reldelim2 = ' |'
+
+/*
+ * Width of the sidebar in pixels. (Do not include px in the value.)
+ * Defaults to 230 pixels.
+ */
+sidebarwidth = 230
+
+/*
+ * Put the sidebar on the right side. Defaults to false.
+ */
+rightsidebar = false
+
+/*
+ * Make the sidebar “fixed” so that it doesn’t scroll out of view for
+ * long body content. This may not work well with all browsers.
+ * Defaults to false.
+ */
+stickysidebar = false
+
+/*
+ * Add an experimental JavaScript snippet that makes the sidebar
+ * collapsible via a button on its side. Doesn’t work
+ * with “stickysidebar”. Defaults to false.
+ */
+collapsiblesidebar = false
+
+/*
+ * Display external links differently from internal links. Defaults to false.
+ */
+externalrefs = false
+
+/*
+ * There are also various color and font options that can change the color
+ * scheme without having to write a custom stylesheet:
+ */
+footerbgcolor    = '#11303d' // Background color for the footer line
+footertextcolor  = '#ffffff' // Text color for the footer line.
+sidebarbgcolor   = '#1c4e63' // Background color for the sidebar
+sidebarbtncolor  = '#3c6e83' // Background color for the sidebar collapse button (used when collapsiblesidebar is true).
+sidebartextcolor = '#ffffff' // Text color for the sidebar
+sidebarlinkcolor = '#98dbcc' // Link color for the sidebar
+relbarbgcolor    = '#133f52' // Background color for the relation bar
+relbartextcolor  = '#ffffff' // Text color for the relation bar
+relbarlinkcolor  = '#ffffff' // Link color for the relation bar
+bgcolor          = '#ffffff' // Body background color
+textcolor        = '#000000' // Body text color
+linkcolor        = '#355f7c' // Body link color
+visitedlinkcolor = '#355f7c' // Body color for visited links
+headbgcolor      = '#f2f2f2' // Background color for headings
+headtextcolor    = '#20435c' // Text color for headings
+headlinkcolor    = '#c60f0f' // Link color for headings
+codebgcolor      = '#eeffcc' // Background color for code blocks
+codetextcolor    = '#333333' // Default text color for code blocks, if not set differently by the highlighting style
+
+// Font for normal text
+bodyfont = 'sans-serif'
+
+// Font for headings
+headfont = "'Trebuchet MS', sans-serif"
